a5b2d0fbba8d2f8b72e465fdb500fa5065b8b2cb
edutekno <54890481+edutekno@users.noreply.github.com>
2023-10-28 13:48:13 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,11 @@
+﻿{
+	"version": 1698474388,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tank-sheet0.png",
+		"images/sprite-sheet0.png"
+	]
+}
